e0dd664a91c6ecaa0051ad5217111b9053fa0d02
vijaychs <vijaychs@dal.ca>
2017-10-02 16:00:02 +0000

    created list

--- task2.py
+++ task2.py
@@ -1,5 +1,13 @@
 import csv
 with open("Crime.csv",'r') as my_file:
- reader = csv.reader(my_file)
- my_list = list(reader)
-print(my_list)
+	reader = csv.reader(my_file)
+# my_list = list(reader)
+#print(my_list)
+crime_type=[]
+crime_id=[]
+crime_count=[]
+	for crime in my_file:
+		if crime == "ASSAULT":
+			print("s")
+		else:
+			print("f")
