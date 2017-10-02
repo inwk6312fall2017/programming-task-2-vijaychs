1a2a39118ff6ebc1ca94de6d211c38d42da14373
vijaychs <vijaychs@dal.ca>
2017-10-02 16:05:35 +0000

    "created using dictionary"

--- task2.py
+++ task2.py
@@ -1,13 +1,10 @@
 import csv
 with open("Crime.csv",'r') as my_file:
-	reader = csv.reader(my_file)
-# my_list = list(reader)
-#print(my_list)
-crime_type=[]
-crime_id=[]
-crime_count=[]
-	for crime in my_file:
-		if crime == "ASSAULT":
-			print("s")
-		else:
-			print("f")
+#	reader = csv.reader(my_file)
+#	my_list = list(reader)
+#	print(my_list)
+
+	reader = csv.DictReader(my_file)
+	data = next(reader)
+print(data)
+
